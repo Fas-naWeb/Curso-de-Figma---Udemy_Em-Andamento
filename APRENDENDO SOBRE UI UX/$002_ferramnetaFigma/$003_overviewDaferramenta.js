d4f7139714c221162cd4792aa6b2f7408ee3b96a
fabiano alves <fasnaweb2020@gmail.com>
2022-04-28 23:48:49 -0300

    Entendendo a ferramenta Figma

---
+++ APRENDENDO SOBRE UI UX/$002_ferramnetaFigma/$003_overviewDaferramenta.js
@@ -0,0 +1,7 @@
+/**
+ * O figma pode ser usado pelo navegador ou voce pode baixar e usar na sua maquina istalado
+ * 
+ * Para baixar ou usar pelo software basta entrar no link do figma.com,voce cria sua conta e passa 
+ * a utiliza-lo 
+ * 
+ */
