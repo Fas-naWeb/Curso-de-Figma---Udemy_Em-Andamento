f9b381f6d018dff6bd85a55d75852668b0ef64e8
fabiano alves <fasnaweb2020@gmail.com>
2022-04-29 15:37:07 -0300

    Anotacoes gerais do figma

---
+++ APRENDENDO SOBRE UI UX/$002_ferramnetaFigma/$004_AnotacoesFigma.js
@@ -0,0 +1,26 @@
+/**
+ *   1° - Para criar um novo arquivo voce clica em Draft +,
+ *   2° - bem no meio da janela onde esta escrito draft/Untiled,voce da dois cliques e renomeia o documento
+ *   3° - No centro fica toda parte onde sera criada as interfaces
+ *   4° - A esquerda fica toda parte de layer,todos compoentes do aplicativo
+ * 
+ * 
+ *   5° - Ja a direita tem toda parte de design,propriedades,prototipo do item que esta selecionado
+ *   6° - Prototipos e o que vai mostrar as telas funcionando com animacao links funcionando
+ *   7° - Inspect tras todo css do que esta sendo montado o palco
+ * 
+ *   8° - Na parte de cima temos icones para colocar,comentarios,textos,caneta,figuras geomericas,frames etc...
+ *        Os frames por exemplo tras varios tamplates prontos tando para desketop,tablet ou smart fone.
+ * 
+ *   9° - Atalhos
+ *         No canto superior direito da tela tem a ferramenta zoom,e interessante sempre deixar em 100%
+ *         que indica o tamho real da tela.
+ * 
+ *   10° - Segura espaco, clicar a tela voce pode segurar e arrastar os componentes da area de trabalho
+ * 
+ *   11° - Segurar o crtl +,aument o zoom e crtl - Diminui o zoom
+ * 
+ *   12° - Segurar o shift e roda a bolinha do mause,voce pode girar para direita ou esquerda conforme o movimento
+ *   
+ *   13° - 
+ */
