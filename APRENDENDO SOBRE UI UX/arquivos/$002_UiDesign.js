be5e31ab4a6c136b3731dd92b53d43c7a97b6cf8
fabiano alves <fasnaweb2020@gmail.com>
2022-04-28 23:05:15 -0300

    Continuacao sobre conceitos basicos de ui e ux design

--- APRENDENDO SOBRE UI UX/arquivos/$002_UiDesign.js
+++ APRENDENDO SOBRE UI UX/arquivos/$002_UiDesign.js
@@ -8,7 +8,7 @@
  *   - Por que?, Quem, Quando,Onde:Como -
  *   Por que ? - Objetivo do nosso cliente,entender a necessidade desse produto
  *   Quem ? -> Entender quem e meu publico alvo,quem vai utilizar esse meu sistema,meu aplicativo.
- *   Quando e Onde? -> Quando o usuario precisa desse produto e onde 
+ *   Quando e Onde? -> Quando o usuario precisa desse produto e onde.
  *   Como? -> Como vamos resolver o problema desse produto utilizando as medidas anteriores. 
  * 
  */
