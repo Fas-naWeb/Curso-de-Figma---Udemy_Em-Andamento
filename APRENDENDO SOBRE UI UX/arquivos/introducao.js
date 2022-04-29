429988a77254d6dc6a5a54761613faad98914af6
fabiano alves <fasnaweb2020@gmail.com>
2022-04-28 22:33:49 -0300

    Conceitos basicos de ui e ux design

---
+++ APRENDENDO SOBRE UI UX/arquivos/introducao.js
@@ -0,0 +1,108 @@
+/**
+ * O QUE E UMA INTERFACE
+ *   E qualquer meio utilizado para controlar um dispositivo e interagir com ele.
+ * 
+ * 
+ * O QUE E EXPERIENCIA DE USUARIO
+ *   Fazer com que o usuario consiga usar o produto e atingir o objetivo que o produto tem
+ *   a oferecer.
+ *   Fazer com que o usuario tenha uma boa experiencia usando o produto.
+ * 
+ * UI - UX
+ *   E necessario que ambos ande juntos,ligando-se com os objetivos de negocios,necessidades do
+ *   isuario e tecnologia.
+ * 
+ *   Entao o resultado e ter uma camada visual alinhada com os objetivos do produto.
+ * 
+ *            
+ * 
+ * 
+ *       --------------------------PIRAMIDE----------------------------------
+ * 
+ *                         -Camadas do UI - UX-
+ *                 ^ 1° - Surface - Camada visual - 
+ *                 | 2° - Skeleton - skeleto do design - 
+ *                 | 3° - Structure - Interacoes e arquitetura do design -
+ *                 | 4° - Scope - Escopo que esse produto tem - 
+ *                 | 5° - Strategy - O que o usuario precisa, o objetivo do produto -
+ * 
+ * 
+
+          Vamos falar um pouco mais sobre todas etapas, mesmo que aqui no curso temos o FOCO maior 
+          em criar interfaces profissionais.
+
+​          5 - Estratégia​
+          Ela consiste no trabalho de investigar quais são as necessidades das pessoas que vão interagir
+          com seu produto/serviço.​
+
+          Nesta etapa, é importante colher (o que o usuário precisa), pois é por meio dessas necessidades
+          que as próximas etapas de todo o processo poderão ser cumpridas.
+
+          Nessa etapa que são levantadas questões importantes como:
+
+
+
+          Qual o motivo do produto, aplicativo ou site?
+
+          Por que o criamos?
+
+          Pra quem estamos fazendo isso?
+
+          Por que as pessoas estão dispostas a usá-lo?
+
+          Por que precisam dele?
+
+
+          4 - Escopo
+          Esse é o momento de definir os requisitos funcionais e de conteúdo para o produto que está sendo
+          desenvolvido. Quais os recursos e conteúdos a aplicação devem ter, quais requisitos ela deve cumprir
+          e como eles estarão alinhados com os objetivos estratégicos da organização.​
+
+          Primeiro o que é requisito? Requisito é uma exigência, solicitação, desejo, necessidade.​
+
+          Requisitos funcionais: Nada mais é que uma requisição de uma função que um software deverá atender/realizar.​
+
+          Requisitos de Conteúdo: São as informações que precisamos para fornecer o valor ao nosso consumidor. Informações como textos, imagens, áudio, vídeos, etc. Sem definir o conteúdo que gostaríamos de mostrar, não temos ideia sobre o tamanho ou o tempo necessário para concluir o projeto.​
+
+
+
+          3 - Estrutura
+          Aqui definimos como o usuário irá interagir com o produto, como o sistema irá se comportar quando
+          o usuário interagir com ele, como ele é organizado e como as informações são priorizadas para facilitar
+          o entendimento. Pensando no design de interação ou seja como usuário vai interagir com o produto e também
+          pensando na arquitetura da informação que basicamente é quais conteúdos serão mostrados na interface, 
+          como vão ser organizados.
+
+          Design de Interação é bem feito, ele:​
+
+          Ajuda os usuários a realizarem seus objetivos.
+
+          Comunica de maneira efetiva como as interações e funcionalidades funcionam.
+
+
+
+          Arquitetura da Informação​:
+
+          Permite que os dados sejam organizados, categorizados e priorizados com base na necessidade do usuário
+          e nos objetivos da organização.
+
+          Que toda a estrutura seja flexível para acomodar possíveis crescimentos e adaptar a mudanças.
+
+          Que as informações sejam mostradas da maneira mais simples para o usuário.​
+
+
+
+          2 - Esqueleto
+          O esqueleto determina a forma visual da tela, a apresentação de todos os elementos que forem necessários
+          para a que a interação ocorra com uma funcionalidade existente na interface. Além disso, é necessário pensar
+          em como as informações serão apresentadas para torná-las efetivas e de fácil entendimento.​
+
+          É nessa etapa onde são criados os famosos wireframes, extremamente úteis para criar um modelo visual de como
+          ficaria uma interface. Ele é um diagrama estático do produto, composto por conteúdo, navegação e como as 
+          interações irão funcionar, sem se preocupar com cores e sim mais no esqueleto como ficaria a interface.
+
+          1 - Superfície
+          É a última camada, mas não menos importante. Por se tratar da camada que está acima de tudo, ela é a soma total
+          dos trabalhos (lembra que falamos na aula o iceberg começamos sempre DE BAIXO PARA CIMA). Esse é o momento onde  
+          determinamos como será o produto e qual será o layout, como será a tipografia, as palheta de cores e etc.​​
+*/
