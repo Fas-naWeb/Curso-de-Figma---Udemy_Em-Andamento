c90d8023baf8e5d4bd4b1471ca0d0eef81b6a0da
fabiano alves <fasnaweb2020@gmail.com>
2022-04-28 23:03:47 -0300

    Conceitos basicos de ui e ux design

---
+++ APRENDENDO SOBRE UI UX/arquivos/$002_UiDesign.js
@@ -0,0 +1,14 @@
+/**'''''''''''''''''''''''''''''''''''''''''''''''''''''''      
+ * UI_DESIGN
+ *  Nao e so criar telas bonitas 
+ *  E um processo para criar interfaces que os usuarios vao usar para 
+ *  interagir com o produto
+ * 
+ * QUE LEVAM AS SEGUINTES PERGUNTAS
+ *   - Por que?, Quem, Quando,Onde:Como -
+ *   Por que ? - Objetivo do nosso cliente,entender a necessidade desse produto
+ *   Quem ? -> Entender quem e meu publico alvo,quem vai utilizar esse meu sistema,meu aplicativo.
+ *   Quando e Onde? -> Quando o usuario precisa desse produto e onde 
+ *   Como? -> Como vamos resolver o problema desse produto utilizando as medidas anteriores. 
+ * 
+ */
